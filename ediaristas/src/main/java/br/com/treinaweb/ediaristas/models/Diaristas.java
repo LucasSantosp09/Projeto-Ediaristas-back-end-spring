6ac367e4bd62fdd32ed2e5b384daa74574a29c91
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-01-10 21:18:05 -0300

    Finalizado modelo diarista

--- ediaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diaristas.java
+++ ediaristas/src/main/java/br/com/treinaweb/ediaristas/models/Diaristas.java
@@ -1,17 +1,44 @@
 package br.com.treinaweb.ediaristas.models;
 
-public class Diaristas {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Diaristas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+    @Column(nullable = false, length = 100)
     private String nomeCompleto;
+    @Column(nullable = false, unique = true, length = 11)
     private String cpf;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false, length = 11)
     private String telefone;
+    @Column(nullable = false)
     private String logradouro;
+    @Column(nullable = false)
     private String numero;
+    @Column(nullable = false)
     private String bairro;
+    @Column(nullable = true)
     private String complemento;
+    @Column(nullable = false, length = 8)
     private String cep;
+    @Column(nullable = false)
+    private String cidade;
+    @Column(nullable = false, length = 2)
+    private String estado;
+    @Column(nullable = false)
     private String codigoIbge;
 
 }
