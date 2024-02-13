5a8a40d5ab8411759dbdfea47a575954fc1ed0a8
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-02-13 20:03:40 -0300

    Filtrar diaristas com base no código IBGE

--- ediaristas/src/main/java/br/com/treinaweb/ediaristas/controller/DiaristaRestController.java
+++ ediaristas/src/main/java/br/com/treinaweb/ediaristas/controller/DiaristaRestController.java
@@ -2,9 +2,11 @@ package br.com.treinaweb.ediaristas.controller;
 
 import br.com.treinaweb.ediaristas.models.Diaristas;
 import br.com.treinaweb.ediaristas.repository.DiaristaRepository;
+import br.com.treinaweb.ediaristas.services.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +17,19 @@ public class DiaristaRestController {
     @Autowired
     private DiaristaRepository diaristaRepository;
 
+    @Autowired
+    private ViaCepService viaCepService;
+
+//    @GetMapping
+//    public List<Diaristas> buscarDiaristasPorCep(){
+//        return diaristaRepository.findAll();
+//    }
+
     @GetMapping
-    public List<Diaristas> buscarDiaristasPorCep(){
-        return diaristaRepository.findAll();
+    public List<Diaristas> buscarDiaristasPorCep(@RequestParam String cep){
+       var endereco = viaCepService.buscarEnderecoPorCep(cep);
+       var codigoIbge = endereco.getIbge();
+       return diaristaRepository.findByCodigoIbge(codigoIbge);
+
     }
 }
--- ediaristas/src/main/java/br/com/treinaweb/ediaristas/repository/DiaristaRepository.java
+++ ediaristas/src/main/java/br/com/treinaweb/ediaristas/repository/DiaristaRepository.java
@@ -3,5 +3,10 @@ package br.com.treinaweb.ediaristas.repository;
 import br.com.treinaweb.ediaristas.models.Diaristas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiaristaRepository extends JpaRepository<Diaristas, Long> {
+
+
+    List<Diaristas> findByCodigoIbge(String codigoIbge);
 }
